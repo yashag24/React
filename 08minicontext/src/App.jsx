ee13071b403cd9c04fb02fde5f160a25e65e9529
Yash Agarwal <agarwaly976@gmail.com>
2025-02-23 19:12:13 +0530

    08contextapi

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/login'
+import Profile from './components/profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+    <h1>React With chai</h1>
+    <Login/>
+    <Profile/>
+    
+    </UserContextProvider>
+  )
+}
+
+export default App
